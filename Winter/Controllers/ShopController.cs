68adb713e77c518a2e7bac1b59e89c6ade9bc419
Onothoghene <marioonosnorbert@gmail.com>
2020-04-06 12:51:59 +0100

    View Update

---
+++ Winter/Controllers/ShopController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Winter.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Category()
+        {
+            return View();
+        }
+    }
+}
--- Winter/ViewModels/RegisterViewModel.cs
+++ Winter/ViewModels/RegisterViewModel.cs
@@ -4,15 +4,16 @@ namespace Winter.ViewModels
 {
     public class RegisterViewModel
     {
-        [Required]
-        [Display(Name = "Name ")]
-        public string UserName { get; set; }
+        //[Required]
+        //[Display(Name = "Name ")]
+        //public string UserName { get; set; }
 
         [Required]
         [EmailAddress]
         public string Email { get; set; }
 
         [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 8)]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
