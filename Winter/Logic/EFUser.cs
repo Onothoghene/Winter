78f226406917154087c2415a41946517a908b194
Onothoghene <marioonosnorbert@gmail.com>
2020-04-10 12:15:14 +0100

    Admin View

---
+++ Winter/Logic/EFUser.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Winter.Data;
+using Winter.Services;
+
+namespace Winter.Logic
+{
+    public class EFUser : IUsers
+    {
+        private readonly WinterContext _context;
+        ModelFactory _modelFactory = new ModelFactory();
+
+
+        public EFUser(WinterContext context)
+        {
+            _context = context;
+        }
+        //public int CountUser()
+        //{
+        //    _context.A
+
+        //    return count;
+        //}
+    }
+}
