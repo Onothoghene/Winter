1b2e2c087ae3881cac6e28902213d15b87919429
Onos <marioonosnorbert@gmail.com>
2021-08-18 23:52:53 +0100

    Cookies completed

--- Winter/Controllers/BaseController.cs
+++ Winter/Controllers/BaseController.cs
@@ -1,13 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Winter.Logic;
+using Winter.Models;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
 
 namespace Winter.Controllers
 {
     public class BaseController : Controller
     {
+        public readonly UserManager<ApplicationUser> UserManager;
+        public readonly SignInManager<ApplicationUser> SignInManager;
+        readonly IUsers _users;
+
+        public BaseController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IUsers users)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+            _users = users;
+
+            //bool isAuthenticated = User.Identity.IsAuthenticated;
+            
+        }
+
+        
+        public async void SetUserIdCookie()
+        {
+            bool IsSignedIn = SignInManager.IsSignedIn(User);
+
+            if (IsSignedIn)
+            {
+                var user = await Task.Run(() => UserManager.GetUserAsync(User));
+                var loggedInUser = _users.GetUserId(user.Email);
+                 HttpContext.Session.SetInt32(UserId, (int)loggedInUser);
+            }
+
+        }
         public string UserId { get; set; } = "UserId";
     }
 }
